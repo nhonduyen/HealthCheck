5ea2d38869d43f016386648391ca83bd71d186dc
duyennguyen <duyenxnguyen@deloitte.ca>
2023-04-06 14:00:31 +0700

    first commit

---
+++ Customer.API/Utility/DatabaseHealthCheck.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Data.SqlClient;
+
+namespace Customer.API.Utility
+{
+    public class DatabaseHealthCheck : IHealthCheck
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseHealthCheck(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var _connectionString = _configuration.GetConnectionString("CustomerDBConnection");
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (SqlException)
+                {
+                    return await Task.FromResult(new HealthCheckResult(context.Registration.FailureStatus, "Cannot connect to Demo.CustomerDB"));
+                }
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
---
+++ Location.API/Persistence/LocationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Location.API.Persistence
+{
+    public class LocationDbContext : DbContext
+    {
+        public LocationDbContext(DbContextOptions<LocationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
